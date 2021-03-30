ef62efce2a6b4f3004728560cf65bef42b6df9de
Katya <ekaterina.leo.nikiforova@gmail.com>
2021-03-30 23:54:36 +0300

    adding content to page using jquery

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
-$(document).ready(function () {
-  alert('jquery loaded!');
-})
+var tweet = `<div style="margin: 20px 0; padding: 10px; background: #eee">
+The big fight live: Ham vs Cheese!
+ </div>`;
+
+$('#tweets div').append(tweet);
